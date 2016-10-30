75d0173d48f32d88f6c2ab84ba050936165efae9
banzhaowu-ausxin <banzhaowu@ausxin.com>
2016-10-30 23:27:08 +0800

    section 4

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var crypto = require('crypto'),
     User = require('../models/user.js'),
-    Post = require('../models/post.js');
+    Post = require('../models/post.js'),
+    multer = require('multer');
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        Post.get(null, function(err, posts){
+        Post.getAll(null, function(err, posts){
             if (err) {
                 posts = [];
             }
@@ -125,6 +126,62 @@ module.exports = function(app) {
         res.redirect('/');
     });
 
+    app.get('/upload', checkLogin);
+    app.get('/upload', function(req, res){
+        res.render('upload', {
+            title: '文件上传',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+
+    app.post('/upload', checkLogin);
+    app.post('/upload', upload.array('field1', 5), function(req, res){
+        req.flash('success', '文件上传成功！');
+        res.redirect('/upload');
+    });
+
+    app.get('/u/:name', function (req, res) {
+      //检查用户是否存在
+      User.get(req.params.name, function (err, user) {
+        if (!user) {
+          req.flash('error', '用户不存在!'); 
+          return res.redirect('/');//用户不存在则跳转到主页
+        }
+        //查询并返回该用户的所有文章
+        Post.getAll(user.name, function (err, posts) {
+          if (err) {
+            req.flash('error', err); 
+            return res.redirect('/');
+          } 
+          res.render('user', {
+            title: user.name,
+            posts: posts,
+            user : req.session.user,
+            success : req.flash('success').toString(),
+            error : req.flash('error').toString()
+          });
+        });
+      }); 
+    });
+
+    app.get('/u/:name/:day/:title', function (req, res) {
+      Post.getOne(req.params.name, req.params.day, req.params.title, function (err, post) {
+        if (err) {
+          req.flash('error', err); 
+          return res.redirect('/');
+        }
+        res.render('article', {
+          title: req.params.title,
+          post: post,
+          user: req.session.user,
+          success: req.flash('success').toString(),
+          error: req.flash('error').toString()
+        });
+      });
+    });
+
     function checkLogin(req, res, next) {
         if (!req.session.user) {
             req.flash('error', '未登录!');
@@ -141,3 +198,15 @@ module.exports = function(app) {
         next();
     }
 };
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb){
+        cb(null, './public/images')
+    },
+    filename: function (req, file, cb){
+        cb(null, file.originalname)
+    }
+});
+var upload = multer({
+    storage: storage
+});
