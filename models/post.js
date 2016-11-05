a1040a82ffa85d62b90d15484357fc43df546fb9
banzhaowu-ausxin <banzhaowu@ausxin.com>
2016-11-05 13:31:42 +0800

    10章节 增加标签和标签页面

--- models/post.js
+++ models/post.js
@@ -1,9 +1,10 @@
 var mongodb = require('./db')
 markdown = require('markdown').markdown;
 
-function Post(name, title, post) {
+function Post(name, title, tags, post) {
     this.name = name;
     this.title = title;
+    this.tags = tags;
     this.post = post;
 }
 
@@ -26,7 +27,9 @@ Post.prototype.save = function(callback) {
         name: this.name,
         time: time,
         title: this.title,
-        post: this.post
+        tags: this.tags,
+        post: this.post,
+        comments: []
     };
     //打开数据库
     mongodb.open(function(err, db) {
@@ -53,8 +56,8 @@ Post.prototype.save = function(callback) {
     });
 };
 
-//读取文章及其相关信息
-Post.getAll = function(name, callback) {
+//一次获取十篇文章
+Post.getTen = function(name, page, callback) {
     //打开数据库
     mongodb.open(function(err, db) {
         if (err) {
@@ -70,18 +73,25 @@ Post.getAll = function(name, callback) {
             if (name) {
                 query.name = name;
             }
-            //根据 query 对象查询文章
-            collection.find(query).sort({
-                time: -1
-            }).toArray(function(err, docs) {
-                mongodb.close();
-                if (err) {
-                    return callback(err); //失败！返回 err
-                }
-                docs.forEach(function(doc) {
-                    doc.post = markdown.toHTML(doc.post);
-                })
-                callback(null, docs); //成功！以数组形式返回查询的结果
+            //使用 count 返回特定查询的文档数 total
+            collection.count(query, function(err, total) {
+                //根据 query 对象查询，并跳过前 (page-1)*10 个结果，返回之后的 10 个结果
+                collection.find(query, {
+                    skip: (page - 1) * 10,
+                    limit: 10
+                }).sort({
+                    time: -1
+                }).toArray(function(err, docs) {
+                    mongodb.close();
+                    if (err) {
+                        return callback(err);
+                    }
+                    //解析 markdown 为 html
+                    docs.forEach(function(doc) {
+                        doc.post = markdown.toHTML(doc.post);
+                    });
+                    callback(null, docs, total);
+                });
             });
         });
     });
@@ -89,6 +99,135 @@ Post.getAll = function(name, callback) {
 
 //获取一篇文章
 Post.getOne = function(name, day, title, callback) {
+    //打开数据库
+    mongodb.open(function(err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //根据用户名、发表日期及文章名进行查询
+            collection.findOne({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, function(err, doc) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                //解析 markdown 为 html
+                if (doc) {
+                    doc.post = markdown.toHTML(doc.post);
+                    doc.comments.forEach(function(comment) {
+                        comment.content = markdown.toHTML(comment.content);
+                    });
+                }
+                callback(null, doc); //返回查询的一篇文章
+            });
+        });
+    });
+};
+
+//返回原始发表的内容（markdown 格式）
+Post.edit = function(name, day, title, callback) {
+    //打开数据库
+    mongodb.open(function(err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //根据用户名、发表日期及文章名进行查询
+            collection.findOne({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, function(err, doc) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, doc); //返回查询的一篇文章（markdown 格式）
+            });
+        });
+    });
+};
+
+//更新一篇文章及其相关信息
+Post.update = function(name, day, title, post, callback) {
+    //打开数据库
+    mongodb.open(function(err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //更新文章内容
+            collection.update({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, {
+                $set: {
+                    post: post
+                }
+            }, function(err) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null);
+            });
+        });
+    });
+};
+
+//删除一篇文章
+Post.remove = function(name, day, title, callback) {
+    //打开数据库
+    mongodb.open(function(err, db) {
+        if (err) {
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+            //根据用户名、日期和标题查找并删除一篇文章
+            collection.remove({
+                "name": name,
+                "time.day": day,
+                "title": title
+            }, {
+                w: 1
+            }, function(err) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null);
+            });
+        });
+    });
+};
+
+//返回所有文章存档信息
+Post.getArchive = function(callback) {
   //打开数据库
   mongodb.open(function (err, db) {
     if (err) {
@@ -100,19 +239,74 @@ Post.getOne = function(name, day, title, callback) {
         mongodb.close();
         return callback(err);
       }
-      //根据用户名、发表日期及文章名进行查询
-      collection.findOne({
-        "name": name,
-        "time.day": day,
-        "title": title
-      }, function (err, doc) {
+      //返回只包含 name、time、title 属性的文档组成的存档数组
+      collection.find({}, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function (err, docs) {
         mongodb.close();
         if (err) {
           return callback(err);
         }
-        //解析 markdown 为 html
-        doc.post = markdown.toHTML(doc.post);
-        callback(null, doc);//返回查询的一篇文章
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+//返回所有标签
+Post.getTags = function(callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //distinct 用来找出给定键的所有不同值
+      collection.distinct("tags", function (err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+//返回含有特定标签的所有文章
+Post.getTag = function(tag, callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      //查询所有 tags 数组内包含 tag 的文档
+      //并返回只含有 name、time、title 组成的数组
+      collection.find({
+        "tags": tag
+      }, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+          return callback(err);
+        }
+        callback(null, docs);
       });
     });
   });
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,25 @@
 var crypto = require('crypto'),
     User = require('../models/user.js'),
     Post = require('../models/post.js'),
+    Comment = require('../models/comment.js'),
     multer = require('multer');
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        Post.getAll(null, function(err, posts){
+        //判断是否是第一页，并把请求的页数转换成 number 类型
+        var page = parseInt(req.query.p) || 1;
+        //查询并返回第 page 页的 10 篇文章
+        Post.getTen(null, page, function(err, posts, total) {
             if (err) {
                 posts = [];
             }
             res.render('index', {
                 title: '主页',
-                user: req.session.user,
                 posts: posts,
+                page: page,
+                isFirstPage: (page - 1) == 0,
+                isLastPage: ((page - 1) * 10 + posts.length) == total,
+                user: req.session.user,
                 success: req.flash('success').toString(),
                 error: req.flash('error').toString()
             });
@@ -107,9 +114,11 @@ module.exports = function(app) {
 
     app.post('/post', checkLogin);
     app.post('/post', function(req, res) {
-        var currentUser = req.session.user;
-            post = new Post(currentUser.name, req.body.title, req.body.post);
-        post.save(function(err){
+        var currentUser = req.session.user,
+            tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+            post = new Post(currentUser.name, req.body.title, tags, req.body.post);
+            
+        post.save(function(err) {
             if (err) {
                 req.flash('error', err);
                 return res.redirect('/');
@@ -127,7 +136,7 @@ module.exports = function(app) {
     });
 
     app.get('/upload', checkLogin);
-    app.get('/upload', function(req, res){
+    app.get('/upload', function(req, res) {
         res.render('upload', {
             title: '文件上传',
             user: req.session.user,
@@ -137,49 +146,175 @@ module.exports = function(app) {
     });
 
     app.post('/upload', checkLogin);
-    app.post('/upload', upload.array('field1', 5), function(req, res){
+    app.post('/upload', upload.array('field1', 5), function(req, res) {
         req.flash('success', '文件上传成功！');
         res.redirect('/upload');
     });
 
-    app.get('/u/:name', function (req, res) {
-      //检查用户是否存在
-      User.get(req.params.name, function (err, user) {
-        if (!user) {
-          req.flash('error', '用户不存在!'); 
-          return res.redirect('/');//用户不存在则跳转到主页
-        }
-        //查询并返回该用户的所有文章
-        Post.getAll(user.name, function (err, posts) {
-          if (err) {
-            req.flash('error', err); 
-            return res.redirect('/');
-          } 
-          res.render('user', {
-            title: user.name,
-            posts: posts,
-            user : req.session.user,
-            success : req.flash('success').toString(),
-            error : req.flash('error').toString()
-          });
+    app.get('/archive', function(req, res) {
+        Post.getArchive(function(err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('archive', {
+                title: '存档',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
         });
-      }); 
     });
 
-    app.get('/u/:name/:day/:title', function (req, res) {
-      Post.getOne(req.params.name, req.params.day, req.params.title, function (err, post) {
+    app.get('/tags', function (req, res) {
+      Post.getTags(function (err, posts) {
         if (err) {
           req.flash('error', err); 
           return res.redirect('/');
         }
-        res.render('article', {
-          title: req.params.title,
-          post: post,
+        res.render('tags', {
+          title: '标签',
+          posts: posts,
           user: req.session.user,
           success: req.flash('success').toString(),
           error: req.flash('error').toString()
         });
       });
+    });
+
+    app.get('/tags/:tag', function (req, res) {
+      Post.getTag(req.params.tag, function (err, posts) {
+        if (err) {
+          req.flash('error',err); 
+          return res.redirect('/');
+        }
+        res.render('tag', {
+          title: 'TAG:' + req.params.tag,
+          posts: posts,
+          user: req.session.user,
+          success: req.flash('success').toString(),
+          error: req.flash('error').toString()
+        });
+      });
+    });
+
+    app.get('/u/:name', function(req, res) {
+        var page = parseInt(req.query.p) || 1;
+        //检查用户是否存在
+        User.get(req.params.name, function(err, user) {
+            if (!user) {
+                req.flash('error', '用户不存在!');
+                return res.redirect('/');
+            }
+            //查询并返回该用户第 page 页的 10 篇文章
+            Post.getTen(user.name, page, function(err, posts, total) {
+                if (err) {
+                    req.flash('error', err);
+                    return res.redirect('/');
+                }
+                res.render('user', {
+                    title: user.name,
+                    posts: posts,
+                    page: page,
+                    isFirstPage: (page - 1) == 0,
+                    isLastPage: ((page - 1) * 10 + posts.length) == total,
+                    user: req.session.user,
+                    success: req.flash('success').toString(),
+                    error: req.flash('error').toString()
+                });
+            });
+        });
+    });
+
+    app.get('/u/:name/:day/:title', function(req, res) {
+        Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('article', {
+                title: req.params.title,
+                post: post,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
+
+    app.post('/u/:name/:day/:title', function(req, res) {
+        var date = new Date(),
+            time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+            date.getHours() + ":" +
+            (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+        var comment = {
+            name: req.body.name,
+            email: req.body.email,
+            website: req.body.website,
+            time: time,
+            content: req.body.content
+        };
+        var newComment = new Comment(
+            req.params.name,
+            req.params.day,
+            req.params.title,
+            comment
+        );
+
+        newComment.save(function(err) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('success', '留言成功!');
+            res.redirect('back');
+        });
+    });
+
+    app.get('/edit/:name/:day/:title', checkLogin);
+    app.get('/edit/:name/:day/:title', function(req, res) {
+        var currentUser = req.session.user;
+        Post.edit(currentUser.name, req.params.day, req.params.title, function(err, post) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            res.render('edit', {
+                title: '编辑',
+                post: post,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
+
+    app.post('/edit/:name/:day/:title', checkLogin);
+    app.post('/edit/:name/:day/:title', function(req, res) {
+        var currentUser = req.session.user;
+        Post.update(currentUser.name, req.params.day, req.params.title, req.body.post, function(err) {
+            var url = encodeURI('/u/' + req.params.name + '/' + req.params.day + '/' + req.params.title);
+            if (err) {
+                req.flash('error', err);
+                return res.redirect(url); //出错！返回文章页
+            }
+            req.flash('success', '修改成功!');
+            res.redirect(url); //成功！返回文章页
+        });
+    });
+
+    app.get('/remove/:name/:day/:title', checkLogin);
+    app.get('/remove/:name/:day/:title', function(req, res) {
+        var currentUser = req.session.user;
+        Post.remove(currentUser.name, req.params.day, req.params.title, function(err) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('back');
+            }
+            req.flash('succss', '删除成功！');
+            res.redirect('/');
+        });
     });
 
     function checkLogin(req, res, next) {
@@ -200,10 +335,10 @@ module.exports = function(app) {
 };
 
 var storage = multer.diskStorage({
-    destination: function (req, file, cb){
+    destination: function(req, file, cb) {
         cb(null, './public/images')
     },
-    filename: function (req, file, cb){
+    filename: function(req, file, cb) {
         cb(null, file.originalname)
     }
 });
