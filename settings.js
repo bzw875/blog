3b152e7aac87a786e42222d1fbb832d927fa5741
bzw875 <bzw875@gmail.com>
2016-11-26 23:13:52 +0800

    delete settings.js

--- settings.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    cookieSecret: 'myBlog',
-    db: 'blog',
-    host: 'localhost',
-    port: 27017,
-    name: 'bzw875',
-    email: 'bzw875@gmail.com',
-    password: '679883bb3fedd5debf5ddb2b49f71ce2'
-};
