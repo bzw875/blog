96659a80bdc24ea178bcdaead18f35b9cb3c3925
bzw875 <bzw875@gmail.com>
2017-01-07 20:11:38 +0800

    删除关于我页面, 添加配置模版文件

--- routes/index.js
+++ routes/index.js
@@ -169,7 +169,7 @@ module.exports = function(app) {
 
     app.get('/about', function (req, res) {
       res.render('about', {
-        title: '关于本站',
+        title: '关于我',
         user: req.session.user,
         success: req.flash('success').toString(),
         error: req.flash('error').toString()
---
+++ settings_template.js
@@ -0,0 +1,9 @@
+module.exports = {
+    cookieSecret: 'you cookie secret',
+    db: 'mongodb database name',
+    host: 'mongodb ip',
+    port: 27017,//mongodb port
+    name: 'you name',
+    email: 'you email',
+    password: 'login password'
+};
