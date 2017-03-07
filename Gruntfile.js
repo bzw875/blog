206a54198263fd24b3935000c8bb9bb005f3d2f6
bzw875 <bzw875@gmail.com>
2017-03-07 23:43:42 +0800

    用 grunt 给静态资源用md5值命名

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        rev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 8
+            },
+            assets: {
+                src: [
+                 'public/javascripts/**/*.js',
+                 'public/stylesheets/**/*.css',
+                ]
+            }
+        },
+        usemin: {
+            css:{
+                files:{
+                    src:['public/stylesheets/**/*.css']
+                }
+            },
+            js:['public/javascripts/**/*.js'],
+            html:['views/**/*.ejs'],
+            options:{
+                assetsDirs: ['static', 'public/'],
+                patterns: {
+                    js: [[/(\/public\/images\/[\/\w-]+\.jpg)/, 'replace image in js']]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-rev');
+    grunt.loadNpmTasks('grunt-contrib-levin-usemin');
+
+    grunt.registerTask('default', ['rev', 'usemin']);
+};
